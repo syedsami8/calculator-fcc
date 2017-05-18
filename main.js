9390804c76e60ac685506056f55c7db448705dcb
Syed Samiuddin <ssamiuddin007@gmail.com>
2017-05-19 04:11:41 +0530

    support for desktop and night mode

--- main.js
+++ main.js
@@ -1,30 +1,107 @@
 $(document).ready(function () {
-    var hit = true;
-    var dotHit = true;
-    var backAt = false;
-    var subhit = false;
-    
+
+    var hit = true; // checks if operator is pressed
+    var dotHit = true; // checks for decimal enter, prevents two decimal points
+    var backAt = false; // i think it is for continuation of answer after equals
+    var subhit = false; // flag used enter minus in division and multiplication
+
+
+    function isDay() {
+
+        var d = new Date();
+        var h = d.getHours();
+        if (h >= 8 && h <= 21)
+            return true;
+        else
+            return false;
+    }
+
+
+    function dayMode() {
+
+
+        $('*').css({
+            'background-color': 'white',
+            'transition': '0.5s ease-out all'
+        });
+        $('.upip').css({
+            'color': 'black',
+            'background-color': 'white'
+        });
+        $('.wholetab input[type=text]').css({
+            'background-color': 'white',
+            'color': 'black'
+        });
+        $('.optab').css('background-color', 'aqua');
+        $('.optab td button').css({
+            'background-color': 'aqua',
+            'color': 'black'
+        });
+        $('.optab tr td').css('background-color', 'aqua');
+        $('.numtab button').css('color', 'rgb(72, 72, 72)');
+
+    }
+
+    function nightMode() {
+        $('*').css({
+            'background-color': 'black',
+            'transition': '0.5s ease-out all'
+        });
+        $('.upip').css({
+            'color': 'white',
+            'background-color': 'black'
+        });
+        $('.wholetab input[type=text]').css({
+            'background-color': 'black',
+            'color': 'white'
+        });
+        $('.optab').css('background-color', '#424242');
+        $('.optab td button').css({
+            'background-color': '#424242',
+            'color': 'white'
+        });
+        $('.optab tr td').css('background-color', '#424242');
+        $('.numtab button').css('color', '#ddd');
+
+    }
+
+
+
+    function isInt(n) {
+        return n % 1 === 0;
+    }
+
+
     function gather() {
         /*
-         * Some Dark Magic is used here!
+         * Some Dark Magic is used here! O_O
          */
         var s = $(".upip").val();
-        var repRegex = /x/g;
-        var numRegex = /(\w+\.\w+|\w+|\-\w+)/g;
-        //var opRegex = /(\+|\*|\-|\/)/g;
-        s = s.replace(repRegex, "*");
-        var opRegex = /((\*|\/)(?=\-\w)|\+|([^\*\/])\-(?=\w))/g;
-        var arr = [];
-        var numResult = s.match(numRegex);
-        var preJoinResult = s.match(opRegex);
-        var opJoin = preJoinResult.join('');
-        var opResult = opJoin.replace(/\w/g,'');
-        opResult = opResult.split('');
-        arr.push(numResult);
-        arr.push(opResult);
-        console.log(numResult);
-        console.log(opResult);
-        return arr;
+        if (s.length >= 3) {
+            console.log($('.upip').val());
+            var repRegex = /x/g;
+            var numRegex = /((\w+\.\w+|\-\w+\.\w+)|\w+|\-\w+)/g;
+            //var opRegex = /(\+|\*|\-|\/)/g;
+            s = s.replace(repRegex, "*");
+            var opRegex = /((\*|\/)(?=\-\w)|(\/)|(\+)|(\*)|([^\*\/])\-(?=\w))/g;
+            var arr = [];
+            var numResult = s.match(numRegex);
+            var preJoinResult = s.match(opRegex);
+            console.log(numResult);
+            console.log(preJoinResult);
+            var opJoin = preJoinResult.join('');
+            var opResult = opJoin.replace(/\w/g, '');
+            opResult = opResult.split('');
+            arr.push(numResult);
+            arr.push(opResult);
+            console.log(numResult);
+            console.log(opResult);
+            return arr;
+        } else {
+            console.log($('.upip').val());
+            return 0;
+        }
+
     }
 
     function execute(num, op) {
@@ -44,7 +121,12 @@ $(document).ready(function () {
             case '*':
                 return num1 * num2;
             case '-':
-                console.log(num1+" "+num2);
+                if (num2 < 0) {
+                    num2 = (num2 * (-1));
+                    //                    console.log(num1 + " " + num2);
+                    return num1 - num2;
+                }
+                //                console.log(num1 + " " + num2);
                 return num1 - num2;
             case '/':
                 if (num2 == 0 && num1 != 0) {
@@ -52,7 +134,11 @@ $(document).ready(function () {
                 } else if (num1 == 0 && num2 == 0) {
                     return 0;
                 } else {
-                    return num1 / num2;
+                    var n = num1 / num2;
+                    if (!isInt(n))
+                        return n.toFixed(5);
+                    else
+                        return n;
                 }
         }
     }
@@ -72,13 +158,16 @@ $(document).ready(function () {
                 case 'o':
                     navigator.vibrate(50);
                     break;
+                case 'ac':
+                    navigator.vibrate(100);
+                    break;
             }
         }
     }
 
     $("button").click(function () {
         var clicked = this.className;
-        
+
         switch (clicked) {
             case 'one':
                 vibrate('n');
@@ -234,32 +323,55 @@ $(document).ready(function () {
             case 'del':
                 vibrate('e');
 
-                $(".upip").val('');
-                //                console.log(lg);
+                //$(".upip").val('');
+                //console.log(lg);
+
+                $('.upip').val($('.upip').val().slice(0, -1));
+
+                var pressTimer;
+
+                $(".del").mouseup(function () {
+                    clearTimeout(pressTimer);
+                    // Clear timeout
+                    return false;
+                }).mousedown(function () {
+                    // Set timeout
+                    pressTimer = window.setTimeout(function () {
+                        vibrate('ac');
+                        $('.upip').val('');
+
+                    }, 500);
+                    return false;
+                });
+
                 hit = true;
                 dotHit = true;
                 subhit = false;
                 break;
             case 'divide':
                 vibrate('o');
-
-                //                console.log(lg);
-                if (hit) {
+                var len = $('.upip').val().length;
+                //                console.log(len);
+                if (hit && len > 0) {
                     $(".upip").val($(".upip").val() + "/");
                     hit = false;
                     dotHit = true;
-                    subhit = true;   
-                }
+                    subhit = true;
+                    backAt = false;
+                } else if (len <= 0) {}
                 break;
             case 'multiply':
                 vibrate('o');
-
+                var len = $('.upip').val().length;
                 //                console.log(lg);
-                if (hit) {
+                if (hit && len > 0) {
                     $(".upip").val($(".upip").val() + "x");
                     hit = false;
                     dotHit = true;
                     subhit = true;
+                    backAt = false;
+                } else if (len <= 0) {
+                    break;
                 }
                 break;
             case 'subtract':
@@ -269,39 +381,55 @@ $(document).ready(function () {
                     $(".upip").val($(".upip").val() + "-");
                     hit = false;
                     dotHit = true;
-                    
-                }else if(subhit){
+                    backAt = false;
+
+                } else if (subhit) {
                     $(".upip").val($(".upip").val() + "-");
                     hit = false;
                     dotHit = true;
                     subhit = false;
+                    backAt = false;
+
                 }
                 break;
             case 'add':
                 vibrate('o');
                 //                console.log(lg);
-
+                console.log(hit);
+                console.log($('.upip').val());
                 if (hit) {
                     $(".upip").val($(".upip").val() + "+");
+                    console.log($('.upip').val());
                     hit = false;
                     dotHit = true;
+                    backAt = false;
                 }
                 break;
             case 'equals':
                 vibrate('e');
 
                 var coll = gather();
-                var num = coll[0];
-                var ops = coll[1];
-                console.log(num);
-                console.log(ops);
-                var result = execute(num, ops);
-                //                console.log(result);
-                $(".upip").val(result);
-                backAt = true;
-                hit = true;
-                dotHit = true;
-                subhit = false;
+                if (coll == 0) {
+                    console.log('empty');
+                } else {
+                    var num = coll[0];
+                    var ops = coll[1];
+                    console.log(num);
+                    console.log(ops);
+                    var result = execute(num, ops);
+                    //                console.log(result);
+                    if (isNaN(result)) {
+                        $(".upip").val(result);
+                    } else {
+                        $(".upip").val(result);
+
+                    }
+                    backAt = true;
+                    hit = true;
+                    dotHit = true;
+                    subhit = false;
+                }
+
                 break;
 
 
@@ -310,5 +438,21 @@ $(document).ready(function () {
         //        console.log(lg);
 
     });
+    
+    var flag = isDay();
+    $('.bulb img').click(function () {
+
+        if (flag) {
+            nightMode();
+            $('.bulb img').attr('src', 'white-bulb.png');
+            flag = false;
+        } else {
+            dayMode();
+            $('.bulb img').attr('src', 'black-bulb.png');
+            flag = true;
+        }
+
+    });
+
 
 });
